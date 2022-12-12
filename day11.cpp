a0e2ad1abc399bb716adfebd6aeddc9e1ea56bf5
robilkot <timur.robilko@gmail.com>
2022-12-12 22:08:58 +0300

    finish day 11

--- day11.cpp
+++ day11.cpp
@@ -2,36 +2,21 @@
 #include <fstream>
 #include <string>
 #include <vector>
+//#define PART1
 
 using namespace std;
 
 struct monkey {
-	vector<int> things;
+	vector<long long> things;
 	char operation;
-	int secondoperand, score, test, dest_true, dest_false;
+	int secondoperand, test, dest_true, dest_false;
+	long long score;
 };
 
-void display(const vector<vector<int>>& m) {
-	for (int i = 0; i < m.size(); i++) {
-		for (int c : m[i]) cout << c << " ";
-		cout << "\n";
-	}
-	cout << "\n\n";
-}
-
-void display(const vector<int>& m) {
-	for (int c : m) cout << c << " ";
-	cout << "\n\n";
-}
-
-void nextround(vector<monkey>& monkeys) {
-	vector<vector<int>> newmonkeys;
-	for (int i = 0; i < monkeys.size(); i++)
-		newmonkeys.push_back({});
-
+void nextround(vector<monkey>& monkeys, int& product) {
 	for (int i = 0; i < monkeys.size(); i++) {
-		while(monkeys[i].things.size()) {
-			monkeys[i].score++;
+		monkeys[i].score += monkeys[i].things.size();
+		while(!monkeys[i].things.empty()) {
 			switch (monkeys[i].operation) {
 			case '*': {
 				if (monkeys[i].secondoperand == -1) monkeys[i].things[0] *= monkeys[i].things[0];
@@ -47,15 +32,12 @@ void nextround(vector<monkey>& monkeys) {
 #ifdef PART1
 			monkeys[i].things[0] /= 3;
 #endif // PART1
+			monkeys[i].things[0] %= product;
 
-			if (monkeys[i].things[0] % monkeys[i].test == 0) {
-				monkeys[monkeys[i].dest_true].things.push_back(monkeys[i].things[0]);
-				monkeys[i].things.erase(monkeys[i].things.begin());
-			}
-			else {
-				monkeys[monkeys[i].dest_false].things.push_back(monkeys[i].things[0]);
-				monkeys[i].things.erase(monkeys[i].things.begin());
-			}
+			if (monkeys[i].things[0] % monkeys[i].test == 0) monkeys[monkeys[i].dest_true].things.push_back(monkeys[i].things[0]);
+			else monkeys[monkeys[i].dest_false].things.push_back(monkeys[i].things[0]);
+
+			monkeys[i].things.erase(monkeys[i].things.begin());
 		}
 	}
 }
@@ -67,8 +49,9 @@ void day11() {
 		return;
 	}
 
-	int currentmonkey = -1, rounds;
+	int currentmonkey = -1, rounds, product = 1;
 	vector<monkey> monkeys;
+
 #ifdef PART1
 	rounds = 20;
 #else 
@@ -78,7 +61,6 @@ void day11() {
 	string command;
 	while (getline(inp, command)) {
 		if (command.empty()) continue;
-		bool nextline = 0;
 		switch (command[2]) {
 		case 'n': {
 			currentmonkey++;
@@ -93,25 +75,26 @@ void day11() {
 		case 'O': {
 			monkeys[currentmonkey].operation = command[23];
 			if (command[25] == 'o') monkeys[currentmonkey].secondoperand = -1;
-			else monkeys[currentmonkey].secondoperand = stoi(command.substr(25, command.size() - 25));
+			else monkeys[currentmonkey].secondoperand = stoi(command.substr(25));
 			break;
 		}
 		case 'T': {
 			monkeys[currentmonkey].test = stoi(command.substr(21, command.size() - 21));
+			product *= monkeys[currentmonkey].test;
 			break;
 		}
 		case ' ': {
-			if (command[7] == 't') monkeys[currentmonkey].dest_true = stoi(command.substr(29, command.size() - 29));
-			else if (command[7] == 'f') monkeys[currentmonkey].dest_false = stoi(command.substr(30, command.size() - 30));
+			if (command[7] == 't') monkeys[currentmonkey].dest_true = stoi(command.substr(29));
+			else if (command[7] == 'f') monkeys[currentmonkey].dest_false = stoi(command.substr(30));
 			break;
 		}
 		}
 	}
 	inp.close();
 
-	for(int i =0; i<rounds;i++) nextround(monkeys);
+	for(int i =0; i<rounds;i++) nextround(monkeys, product);
 
-	int firstmonkeyscore = 0, secondmonkeyscore = 0;
+	long long firstmonkeyscore = 0, secondmonkeyscore = 0;
 	for (int i = 0; i < monkeys.size(); i++) if (monkeys[i].score > firstmonkeyscore) firstmonkeyscore = monkeys[i].score;
 	for (int i = 0; i < monkeys.size(); i++) if (monkeys[i].score > secondmonkeyscore && monkeys[i].score < firstmonkeyscore) secondmonkeyscore = monkeys[i].score;
 
